2199e64bb1d8e6be17b487481fc40a0a206bb071
sim-likes-pi <sim3.14159cuber@gmail.com>
2026-07-30 21:47:15 -0600

    Basic layout

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
-import textual
+from textual.app import App, ComposeResult
+from textual.containers import HorizontalGroup, VerticalScroll
+from textual.widgets import Button, Digits, Footer, Header, Label
 
-def main():
-    print("Hello from brainfoo!")
+
+class TimeDisplay(Label):
+    """A widget to display elapsed time."""
+
+
+class Stopwatch(HorizontalGroup):
+    """A stopwatch widget."""
+
+    def compose(self) -> ComposeResult:
+        """Create a single line of cells"""
+
+        for i in range(10):
+            yield Label("000", variant="cell_label")
+
+class StopwatchApp(App):
+    """A Textual app to manage stopwatches."""
+
+    CSS_PATH = "style.css"
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Footer()
+        yield VerticalScroll(Stopwatch(), Stopwatch(), Stopwatch())
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.theme = (
+            "textual-dark" if self.theme == "textual-light" else "textual-light"
+        )
 
 
 if __name__ == "__main__":
-    main()
+    app = StopwatchApp()
+    app.run()
