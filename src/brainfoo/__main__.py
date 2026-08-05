cc2a5ecb69cc0487752a8f0d16ac13b79c3ad08e
sim-likes-pi <sim3.14159cuber@gmail.com>
2026-08-05 02:05:00 -0600

    updated logo

--- src/brainfoo/__main__.py
+++ src/brainfoo/__main__.py
@@ -1,15 +1,24 @@
 from .app import BrainfooApp
 import argparse
 
+print("""
+    \033[35;1m,-~-^*-**^~~.\033[0m
+   \033[35;1m/             \\\033[0m
+  \033[35;1m(    \033[32;1m.\033[0;35m \033[32;1;5m>\033[0;35m \033[32;1m.\033[0;35m \033[32;1m.\033[35m    |\033[0m
+  \033[35;1m|    \033[30;43mf\033[0;1;35m   \033[30;43mo\033[0;1;35m \033[30;43mo\033[0;1;35m    )\033[0m
+  \033[35;1m(          _.-./\033[0m
+   \033[35;1m\\        /*\033[0m
+    \033[35;1m~%/__~*^\033[0m
+     \033[35;1m,%\033[0m
+""")
+
 parser = argparse.ArgumentParser(
                     prog='brainfoo',
                     description='A TUI Brainfk debugger',
                     epilog='https://github.com/Sim3-14159/Brainfoo')
 
 parser.add_argument('filename', nargs='?', default=None) 
-
 args = parser.parse_args()
-print(args.filename)
 
 def main() -> None:
     BrainfooApp().run()
