a652584b7d5be63e5f658729ecd20e3d925946c1
sim-likes-pi <sim3.14159cuber@gmail.com>
2026-08-05 18:54:35 -0600

    clean interpreter.py formatting

--- src/brainfoo/interpreter.py
+++ src/brainfoo/interpreter.py
@@ -1,22 +1,29 @@
+"""
+Brainfk interpreter
+"""
+
 class BrainfkError(BaseException):
+    """Custom error class for exceptions in Brainfk execution"""
     def __init__(self, reason):
         self.reason = reason
 
 class BrainfkInterpreter:
+    """Brainfk interpreter/debugger"""
     def __init__(self):
         """Initialize self"""
         self.index = 0
 
     def run(self, code: str) -> str:
+        """Run Brainfk code"""
         if (l := code.count("[")) != (r := code.count("]")):
             raise BrainfkError(f"IMBALANCED NUMBER OF BRACKETS | '['x{l} / ']'x{r}")
-        
-        for i, char in enumerate(code):
+
+        for _, char in enumerate(code):
             if char == "<":
                 self.index -= 1
             elif char == ">":
                 self.index += 1
-            
+
             if self.index < 0:
                 raise BrainfkError("INDEX UNDERFLOW | TOO MANY '<'S")
         return "Hello World!\n"
